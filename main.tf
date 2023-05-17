61cd0d11d8191d0829c66a0dc0567bf5189b3d5d
Khant Naing Set <khantnaingset@outlook.com>
2023-05-17 22:27:06 +0630

    add code review bulletin points in README.md

--- main.tf
+++ main.tf
@@ -47,16 +47,3 @@ resource "aws_instance" "instance1" {
   }
 }
 
-output "subnet_id" {
-  value = aws_subnet.instance_subnet.id
-}
-
-output "aws_vpc_id" {
-  value = aws_vpc.instance_vpc.id
-  
-}
-
-output "sg_id" {
-  value = aws_security_group.instance_security_group.id
-  
-}
